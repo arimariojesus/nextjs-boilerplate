f5c3cbc7f61063a6053e78fed3510d49d5717021
arimariojesus <arimario.jesus@hotmail.com>
2023-06-24 17:58:07 -0300

    refactor: remove css files

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   width: 100%;
   height: 100%;
-  background-color: #0071ff33;
+  background-color: var(--background);
 
   display: flex;
   align-items: center;
   justify-content: center;
+
+  color: var(--white);
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #a67926;
+    --background: #030518;
+    --white: #eee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -17,6 +29,16 @@ const GlobalStyles = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `;
 
